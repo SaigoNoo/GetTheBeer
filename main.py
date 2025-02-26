5d7203ac39e761b7684bf2cb24abdcf9697dc99c
SaigoNoo <giorgiodoussis@gmail.com>
2025-02-27 00:44:48 +0100

    Interaction entre la base de donnée et FastAPI (a essayer avec une bdd local de l'étudiant) ! Des corrections et ajustements seront a apporter aux prochaines itérations !

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
-from argparse import ArgumentParser
 from os.path import exists
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from mysql.connector import connect, Error
 
-# ARGS
-arg = ArgumentParser()
-arg.add_argument("-me", "--make-env", action="store_true", help="Créer un fichier .env")
-arg.add_argument("-ei", "--env-intuitive", action="store_true", help="Remplir le .env depuis ici...")
-arg = arg.parse_args()
 
+class Validation:
+    @staticmethod
+    def master_login_valid(password: str) -> bool:
+        return Env().get_value(key="BACKEND_MASTER_PASSWORD") == password
 
-def press_exit():
-    input(" > Pressez Enter pour continuer...")
-    exit()
+    @staticmethod
+    def have_master_password() -> bool:
+        return "BACKEND_MASTER_PASSWORD" in Env().get_value()
+
+    @staticmethod
+    def env_exist() -> bool:
+        return exists(".env")
 
 
 class Env:
@@ -31,24 +34,35 @@ class Env:
         self.dbname = dbname
         self.port = "3306" if port == "" else port
 
-    def store_values(self):
+    def store_values(self, master_password: str):
         temp = (
             f"DB_SERVER={self.server}\n"
             f"DB_USERNAME={self.username}\n"
             f"DB_PASSWORD={self.password}\n"
             f"DB_NAME={self.dbname}\n"
-            f"DB_PORT={self.port}"
+            f"DB_PORT={self.port}\n"
+            f"BACKEND_MASTER_PASSWORD={master_password}"
         )
         with open(file=".env", mode="w", encoding="utf-8") as env:
             return env.write(temp)
+
+    def set_master_password(self, password: str):
+        datas = self.get_value()
+        text = ""
+        datas["BACKEND_MASTER_PASSWORD"] = password
+        for element in datas:
+            text += f"{element}={datas[element]}\n"
+        text = text[:-1]
+        with open(file=".env", mode="w", encoding="utf-8") as env:
+            env.write(text)
 
     @staticmethod
     def get_value(key: str = None):
         temp = {}
         with open(file=".env", mode="r", encoding="utf-8") as env:
             for line in env.readlines():
-                key, value = line.split("=")
-                temp[key.strip()] = value.strip()
+                key_dict, value_dict = line.strip().split("=")
+                temp[key_dict.strip()] = value_dict.strip()
         return temp if key is None else temp[key]
 
 
@@ -56,15 +70,9 @@ class Database:
     def __init__(self):
         self.socket = None
         self.__cursor = None
-        self.env_exist()
-
-    @staticmethod
-    def env_exist():
-        if not exists(".env"):
-            print(" > Votre fichier .env n'existe pas, utilisez --make-env pour créer un .env")
-            press_exit()
 
     def connect(self):
+        print(f" > Tentative de connexion avec {Env().get_value(key='DB_USERNAME')}@{Env().get_value(key='DB_SERVER')}")
         try:
             connection = connect(
                 host=Env().get_value(key="DB_SERVER"),
@@ -81,42 +89,114 @@ class Database:
             return True
         except Error as err:
             print(f" > {err}")
-            press_exit()
         except KeyError as err:
             print(f" > Erreur de clé: {err} n'existe pas dans .env ou le champ est vide !")
-            press_exit()
 
-    def show_tables(self):
-        return self.__cursor.execute("SHOW TABLES;")
+    def login_valid(self, username: str, password: str):
+        response = {}
+        self.__cursor.execute("SELECT username, password FROM `gtb_users` WHERE username = %s;", (username,))
+        rows = self.__cursor.fetchall()
+        if len(rows) > 0:
+            if rows[0][1] == password:
+                response["success"] = True
+                response["message"] = "Connexion avec succès..."
+            else:
+                response["success"] = False
+                response["message"] = "Les identifiants sont erronés !"
+        else:
+            response["success"] = False
+            response["message"] = "Les identifiants sont erronés !"
 
+        return response
 
-if arg.make_env:
-    print(f" > Création du fichier .env {'en mode intuitif' if arg.env_intuitive else ''}")
-    if arg.env_intuitive:
-        env = Env(
-            server=input(" > Adresse vers le serveur mySQL [localhost]: "),
-            username=input(" > Nom d'utilisateur: "),
-            password=input(" > Mot de passe: "),
-            dbname=input(" > Nom de la base de donnée: "),
-            port=input(" > Port d'écoute de la base de donnée [3306]: ")
-        )
-    else:
-        env = Env()
-    env.store_values()
 
 app = FastAPI()
-db = Database()
-db.connect()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],  # Autorise toutes les méthodes (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Autorise tous les headers
+)
 
-print(" > Vérification de si la DB est complète...")
-print(" > Vérifications finies...")
+valid = Validation()
+
+if not valid.env_exist():
+    Env().store_values(master_password="getthebeer")
+
+if valid.env_exist() and valid.have_master_password():
+    db = Database()
+    db.connect()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# API Calls
+@app.get(
+    path="/admin/env/set",
+    tags=["Admin | Env"]
+)
+async def env_set(
+        username: str,
+        password: str,
+        database: str,
+        master_password: str,
+        port: str = "3306",
+        server: str = "localhost"
+):
+    response = {}
+    if valid.have_master_password() and valid.master_login_valid(password=master_password):
+        response["success"] = True
+        response["message"] = f"Le .env a bien été modifié !"
+        Env(
+            server=server,
+            username=username,
+            password=password,
+            dbname=database,
+            port=port
+        ).store_values(master_password=master_password)
+    else:
+        response["success"] = False
+        response["message"] = "Le MASTER_PASSWORD est erroné, vide où n'a pas été déclaré !"
+    return response
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get(
+    path="/admin/env/change_master_password",
+    tags=["Admin | Env"]
+)
+async def change_passwd(old_password: str, new_password, confirm_password: str):
+    response = {}
+    # Si le nouveau mot de passe est vide
+    if len(new_password) == 0:
+        response["success"] = False
+        response["message"] = "Le nouveau mot de passe est vide ! Valeur obligatoire !"
+
+    # Si l'ancien mot de passe est validé
+    if valid.master_login_valid(password=old_password):
+        if new_password == confirm_password:
+            response["success"] = True
+            response["message"] = "Le MASTER PASSWORD a bien été modifié !"
+            Env().set_master_password(password=new_password)
+        else:
+            response["success"] = False
+            response["message"] = "Le nouveau mot de passe n'est pas le même qu'a la confirmation !"
+    else:
+        response["success"] = False
+        response["message"] = "L'ancien master password est incorrect !"
+    return response
+
+
+# A CHANGER: Passer en mode POST pour utiliser LoginRequest par question de sécurité
+"""
+La fonction try_login() utilise fetch() pour envoyer des données à l’API, mais elle a plusieurs problèmes :
+Les données sensibles (password) sont envoyées en GET → Mauvaise pratique car elles peuvent être stockées dans les logs du serveur ou visibles dans l'URL.
+Aucune gestion de réponse → One ne sait pas si le serveur a bien reçu la requête ou s'il y a une erreur.
+Il faut utiliser POST avec JSON pour la sécurité.
+"""
+
+
+@app.get(
+    path="/api/login",
+    tags=["Core"]
+)
+async def login(username: str, password: str):
+    return db.login_valid(username=username, password=password)
