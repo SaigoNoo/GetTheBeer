ee7a9ed2bce0064fb0343e7fbd957e80db7f494e
SaigoNoo <giorgiodoussis@gmail.com>
2025-03-25 15:49:32 +0100

    #10
    Le code permet de vérifier si l'utilisateur a déjà introduit une demande de reset ! La base de donnée supprime automatiquement les demandes de reset au bout de 15minutes, via une stratégie trigger de la DB. (Sera prochainement uploadé !)

--- api/user.py
+++ api/user.py
@@ -9,11 +9,9 @@ from classes.mail import Mail
 
 class Users:
     def __init__(self, db: Database):
-        self.reset_password_pool = {
-        }
         self.db = db
 
-    def get_email_from_username(self, username: str):
+    def get_email_from_username_or_email(self, username: str):
         if self.db.call_function(name="user_exists", username=username) == 1:
             id_u = self.db.call_function(name="id_of_user", username=username)
             return self.db.call_function(name="get_user_info_by_id", to_json=True, id=id_u)["email"]
@@ -24,11 +22,19 @@ class Users:
 
     def ask_reset(self, email: str):
         if self.db.call_function(name="user_exists", email_username=email):
+            id_u = self.db.call_function(name="id_of_user", email_username=email)
             if "@" not in email:
-                email = self.get_email_from_username(username=email)
-            Mail(receiver=email, title="Demande de reset", body="VOILA").send()
+                email = self.get_email_from_username_or_email(username=email)
+            if not self.db.call_function(name="is_active_token", id=id_u) == 1:
+                url_reset = f"https://localhost:8000/api/reset/token/{id_u}"
+                message = ("Vous avez fait une demande réinitialisation de mot de passe.\n"
+                           f"Rendez vous sur {url_reset}\n")
+                Mail(receiver=email, title="Demande de reset", body=message).send()
+                return "Un mail à été envoyé si votre compte existe !"
+            else:
+                return "Une demande a déjà été effectuée, veuillez patienter 15min à compter du premier reset !"
         else:
-            return False
+            return "Erreur ou compte inconnu !"
 
     def reset_password(self, username: str, token: str):
         pass
