7e4eba00660a6f6e7d21f98f9e8a07dd999b2eee
roquetar <arnaud.roquet2003@gmail.com>
2025-04-15 13:38:00 +0200

    création des tables dans le bon ordre et rajout de niveau 1 par défaut

--- backend/files_db/instructions.sql
+++ backend/files_db/instructions.sql
@@ -1,5 +1,17 @@
 ------------------ Création des tables --------------------
 
+CREATE TABLE titre (
+    title_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title_name VARCHAR(100)
+);
+
+CREATE TABLE niveaux (
+    ID_level INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title_ID INT UNSIGNED NOT NULL,
+    min_bieres INT,
+    FOREIGN KEY (title_ID) REFERENCES titre(title_ID) ON DELETE CASCADE
+);
+
 CREATE TABLE utilisateurs (
     user_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(100),
@@ -11,7 +23,7 @@ CREATE TABLE utilisateurs (
     biographie TEXT,
     reserve_biere INT DEFAULT 0,
     biere_gagnee INT DEFAULT 0,
-    ID_level INT UNSIGNED NOT NULL,
+    ID_level INT UNSIGNED NOT NULL DEFAULT 1,
     FOREIGN KEY (ID_level) REFERENCES niveaux(ID_level) ON DELETE CASCADE
 );
 
@@ -25,15 +37,6 @@ CREATE TABLE amis (
 );
 
 
-CREATE TABLE duel (
-    user_ID INT UNSIGNED NOT NULL,
-    Game_ID INT UNSIGNED NOT NULL,
-    PRIMARY KEY (user_ID, Game_ID),
-    FOREIGN KEY (user_ID) REFERENCES utilisateurs(user_ID) ON DELETE CASCADE,
-    FOREIGN KEY (Game_ID) REFERENCES jeux(Game_ID) ON DELETE CASCADE
-);
-
-
 CREATE TABLE jeux (
     Game_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     date DATETIME,
@@ -43,32 +46,27 @@ CREATE TABLE jeux (
 );
 
 
-CREATE TABLE niveaux (
-    ID_level INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title_ID INT UNSIGNED NOT NULL,
-    min_bieres INT,
-    FOREIGN KEY (title_ID) REFERENCES titre(title_ID) ON DELETE CASCADE
-);
-
-CREATE TABLE titre (
-    title_ID INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title_name VARCHAR(100)
+CREATE TABLE duel (
+    user_ID INT UNSIGNED NOT NULL,
+    Game_ID INT UNSIGNED NOT NULL,
+    PRIMARY KEY (user_ID, Game_ID),
+    FOREIGN KEY (user_ID) REFERENCES utilisateurs(user_ID) ON DELETE CASCADE,
+    FOREIGN KEY (Game_ID) REFERENCES jeux(Game_ID) ON DELETE CASCADE
 );
 
 
 ------------------ Ajout des données de base --------------------
 
-
-INSERT INTO utilisateurs (nom, prenom, pseudo, mail, image, motdepasse, biographie, ID_level) VALUES
-('Valcke', 'Tristan', 'Trisouille', 't.valcke@students.ephec.be', 'tris.png', 'user123', 'Voici ma bio', 1),
-('Preat', 'Thomas', 'TomPrt', 'thomas.preat@students.ephec.be', 'thomas.png', 'preat123', 'Toujours le sourire !', 1),
-('Roquet', 'Arnaud', 'Roqnoob', 'arnaud.roquet@students.ephec.be', 'arnaud.jpg', 'roquet456', 'Master en dégustation de pils.', 1),
-('Doussis', 'Giorgios', 'GeoTheGreek', 'giorgios.doussis@students.ephec.be', 'giorgios.png', 'greece789', 'J’amène le soleil avec moi.', 1),
-('Kwizera', 'Dorian', 'DKwiz', 'dorian.kwizera@students.ephec.be', 'dorian.jpg', 'kwz321', 'Toujours partant pour un défi.', 1);
+INSERT INTO titre (title_name) VALUES ('Nouveau venu');
 
 INSERT INTO niveaux (title_ID, min_bieres) VALUES (1, 0);
 
-INSERT INTO titre (title_name) VALUES ('Nouveau venu');
+INSERT INTO utilisateurs (nom, prenom, pseudo, mail, image, motdepasse, biographie) VALUES
+('Valcke', 'Tristan', 'Trisouille', 't.valcke@students.ephec.be', 'tris.png', 'user123', 'Voici ma bio'),
+('Preat', 'Thomas', 'TomPrt', 'thomas.preat@students.ephec.be', 'thomas.png', 'preat123', 'Toujours le sourire !'),
+('Roquet', 'Arnaud', 'Roqnoob', 'arnaud.roquet@students.ephec.be', 'arnaud.jpg', 'roquet456', 'Master en dégustation de pils.'),
+('Doussis', 'Giorgios', 'GeoTheGreek', 'giorgios.doussis@students.ephec.be', 'giorgios.png', 'greece789', 'J’amène le soleil avec moi.'),
+('Kwizera', 'Dorian', 'DKwiz', 'dorian.kwizera@students.ephec.be', 'dorian.jpg', 'kwz321', 'Toujours partant pour un défi.');
 
 
 ------------------ Procédures et Fonctions --------------------
